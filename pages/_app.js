af670778a7fef073570f9c149194f042bbc426cc
Lucas-Toscano <lucas_toscano00@hotmail.com>
2021-09-09 20:37:17 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,48 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-4/5 mx-auto text-center my-8">
+      <header>
+        <h1 className="text-6xl font-semibold font-sans">
+          <Link href="/">
+            <a className="text-black hover:text-gray-600">RPG da Discórdia</a>
+          </Link>
+        </h1>
+
+        <nav className="my-6 text-2xl">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/starwars">
+                <a>Star Wars</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/quest">
+                <a>Quest</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/witcher">
+                <a>Witcher</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/dnd">
+                <a>D&D</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;

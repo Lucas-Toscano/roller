b74424a5bd381565fbc79e7a19a59aa462c07863
Lucas-Toscano <lucas_toscano00@hotmail.com>
2022-08-03 20:29:10 -0300

    modifier & bonus js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,51 @@
-// const d20 = Math.floor(Math.random() * 20);
-function modifier() {
-    const mod = document.getElementById("modifier").value;
-}
 
-const modifi = modifier();
-const bonifi = bonus();
 
-function bonus() {
-  const bon = document.getElementById("bonus").value;
-}
+
+
 
 const diceResult = (min, max) => Math.floor(Math.random() *(max - min) + 1);
 
 const button = document.querySelector('.rollDice');
 const result = document.querySelector('.rolledNumber');
+const sum = document.querySelector('.sum');
 const diceAlert = document.querySelector('.diceAlert');
 
-button.onclick = function() {
-  const diceRoll = diceResult(1, 21);
+const modifier = document.querySelector('.modifier');
+const bonus = document.querySelector('.bonus');
+const profBonus = document.querySelector('.profBonus');
 
-  if (diceRoll === 20) {
+button.onclick = function() {
+  const d20 = diceResult(1, 21);
+  const d4 = diceResult(1, 5);
+  const d6 = diceResult(1, 7);
+  const d8 = diceResult(1, 9);
+  const d10 = diceResult(1, 11);
+  const d12 = diceResult(1, 13);
+  const xd20 = diceResult(1, 21);
+
+  const mod = parseFloat(modifier.value);
+  const bon = parseFloat(bonus.value);
+  const profBon = parseFloat(profBonus.value);
+
+  console.log(mod);
+
+  if (d20 === 20) {
     diceAlert.innerHTML = 'Uau! um acerto crítico!';
-  } else if (diceRoll === 1) {
+  } else if (d20 === 1) {
     diceAlert.innerHTML = 'Falha crítica!';
   } else {
     diceAlert.innerHTML = '';
   }
 
-  if(modifier(modifi) === '0' && bonus(bonifi) === '0') {
-    
+  const totalSum = d20 + mod + profBon + bon;
+
+  if (totalSum === 0) {
+    result.innerHTML = '1';
+  } else {
+    result.innerHTML = `${d20 + mod + bon + profBon}`;
   }
-  result.innerHTML = `${diceRoll}`;
+  sum.innerHTML = `${d20} + ${mod} + ${profBon} + ${bon} `;
+
   
 };
 
